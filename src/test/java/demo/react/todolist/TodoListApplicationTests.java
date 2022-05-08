67bb8578cfd5413aa572689f70aa9920f6c75d42
Martin Gonzalez <martineduardogonzalez@gmail.com>
2022-05-08 14:28:38 -0300

    Seting up mySql

--- src/test/java/demo/react/todolist/TodoListApplicationTests.java
+++ src/test/java/demo/react/todolist/TodoListApplicationTests.java
@@ -10,7 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TodoListApplicationTests {
 
 	@Test
-	public void contextLoads() {
+//	public void contextLoads() {
+//	}
+
+	public void test() {
+		System.out.println("test");
 	}
+
+
 
 }
